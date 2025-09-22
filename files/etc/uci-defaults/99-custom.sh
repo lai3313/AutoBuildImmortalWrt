3c73700578e0f36c0b1f3d2e66643f0652b4083d
lai3313 <jwdlhhm@gmail.com>
2025-09-22 19:22:52 +0800

    Update 99-custom.sh
    wifi驱动检测，删除网络接口无线显示

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -3,6 +3,14 @@
 # Log file for debugging
 LOGFILE="/etc/config/uci-defaults-log.txt"
 echo "Starting 99-custom.sh at $(date)" >>$LOGFILE
+
+if opkg list-installed | grep -q "kmod-brcmfmac"; then
+    echo "✅ WiFi 驱动已安装，保留无线配置" >> $LOGFILE
+else
+    echo "❌ WiFi 驱动未安装，移除无线配置" >> $LOGFILE
+    rm -f /etc/config/wireless
+fi
+
 # 设置默认防火墙规则，方便单网口虚拟机首次访问 WebUI 
 # 因为本项目中 单网口模式是dhcp模式 直接就能上网并且访问web界面 避免新手每次都要修改/etc/config/network中的静态ip
 # 当你刷机运行后 都调整好了 你完全可以在web页面自行关闭 wan口防火墙的入站数据
